232664c333bb16794096b14046f0b1c0cfec2a10
FWRobins <FW.Robins@gmail.com>
2025-03-17 15:13:53 +0200

    2503171513 added FPS limit

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -22,6 +24,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
