e81b116c68053b0ad285948ca1ed0129917d0bce
FWRobins <FW.Robins@gmail.com>
2025-03-17 15:41:02 +0200

    2503171540 added player class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -15,6 +16,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
 
@@ -23,6 +25,7 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
